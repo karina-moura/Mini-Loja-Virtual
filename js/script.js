65ba86d09a167410dace2101902a9372ed024232
Karina  Moura <83098168+karina-moura@users.noreply.github.com>
2022-02-25 15:11:52 -0300

    finalizando

--- js/script.js
+++ js/script.js
@@ -31,7 +31,16 @@ inicializarLoja = () => {
 inicializarLoja();
 
 atualizarCarrinho = () => {
-    console.log(itens);
+    var containerCarrinho = document.getElementById('carrinho');
+    containerCarrinho.innerHTML = "";
+    itens.map((val)=>{
+        if(val.quantidade > 0){
+        containerCarrinho.innerHTML += `
+       <p>`+val.nome+` | quantidade: `+val.quantidade+`</p>
+       <hr>
+       `;
+       }
+    })
 
 }
 
