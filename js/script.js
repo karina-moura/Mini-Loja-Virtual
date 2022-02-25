6f52e76b02a1c0880d524ce33fba31b8c05c2d56
Karina  Moura <83098168+karina-moura@users.noreply.github.com>
2022-02-25 14:39:54 -0300

    Adicionando estilização

--- js/script.js
+++ js/script.js
@@ -1,8 +1,46 @@
 const itens=[
+    
     {
-        id: 0,
-        nome: 'camiseta',
+        id: 1,
+        nome: 'tenis',
+        img: 'image-icon.jpg',
+        quantidade: 0
+    },
+    {
+        id: 2,
+        nome: 'calça',
         img: 'image-icon.jpg',
         quantidade: 0
     },
 ]
+
+inicializarLoja = () => {
+    var containerProdutos = document.getElementById('produtos');
+    itens.map((val)=>{
+        containerProdutos.innerHTML += `
+       <div class="produto-single">
+       <img src="`+val.img+`" />
+       <p>`+val.nome+`</p>
+       <a key="`+val.id+`" href="#">Adicionar ao carrinho</a>
+       </div>
+       `;
+    })
+
+}
+
+inicializarLoja();
+
+atualizarCarrinho = () => {
+    console.log(itens);
+
+}
+
+var links = document.getElementsByTagName('a');
+for(var i = 0; i < links.length; i++){
+    links[i].addEventListener("click", function(){
+        let key = this.getAttribute('key');
+        itens[key].quantidade++;
+        atualizarCarrinho();
+        return false;
+    })
+}
